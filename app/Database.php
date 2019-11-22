a1521c42c90d575f92e3496a064b83d58e7f731c
Ananye Agarwal <ananayagarwal@gmail.com>
2019-11-22 15:14:36 +0530

    Added a Database class to avoid code repetition

---
+++ app/Database.php
@@ -0,0 +1,54 @@
+<?php
+
+class Database {
+    private $db_conn;
+    public $success;
+
+    function connect() {
+        $this->db_conn = OCILogon("ora_anag004", "a23835341", "dbhost.students.cs.ubc.ca:1522/stu");
+        if ($this->db_conn) {
+            echo "Connected to DB...<br>";
+            return true;
+        } else {
+            $e = OCI_Error(); // For OCILogon errors pass no handle
+            echo htmlentities($e['message']);
+            return false;
+        }
+    }
+
+    // Executes a plain SQL command, returns the result
+    function executePlainSQL($cmdstr) {
+        $statement = OCIParse($this->db_conn, $cmdstr);
+
+        if (!$statement) {
+            echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+            $e = OCI_Error($this->db_conn); // For OCIParse errors pass the connection handle
+            echo htmlentities($e['message']);
+            $this->success = False;
+        }
+
+        $r = OCIExecute($statement, OCI_DEFAULT);
+        
+        // ========= CODE TO DEBUG RETRIEVED DATA ==========
+        // OCI_fetch_all($statement, $res);
+        // echo "<pre>\n";
+        // var_dump($res);
+        // echo "</pre>\n";
+        // =================================================
+
+        if (!$r) {
+            echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+            $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+            echo htmlentities($e['message']);
+            $this->success = False;
+        }
+        
+        return $statement;
+    }
+
+    function __destruct() {
+        OCI_close($this->db_conn);
+    }
+ }
+
+?>
--- app/cars.php
+++ app/cars.php
@@ -6,67 +6,7 @@
     <body>
         <!-- A drop down menu listing the car types -->
         <?php
-            $db_conn = NULL;
-            $success = True;
-
-            // Connects to the database, returns a true/false value
-            function connectToDB() {
-                global $db_conn;
-
-                $db_conn = OCILogon("ora_anag004", "a23835341", "dbhost.students.cs.ubc.ca:1522/stu");
-                if ($db_conn) {
-                    echo "Connected to DB...<br>";
-                    return true;
-                } else {
-                    $e = OCI_Error(); // For OCILogon errors pass no handle
-                    echo htmlentities($e['message']);
-                    return false;
-                }
-            }
-
-            // Executes a plain SQL command, returns the result
-            function executePlainSQL($cmdstr) {
-                global $db_conn, $success;
-
-                $statement = OCIParse($db_conn, $cmdstr);
-
-                if (!$statement) {
-                    echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-                    $e = OCI_Error($db_conn); // For OCIParse errors pass the connection handle
-                    echo htmlentities($e['message']);
-                    $success = False;
-                }
-
-                $r = OCIExecute($statement, OCI_DEFAULT);
-                
-                // ========= CODE TO DEBUG RETRIEVED DATA ==========
-                // OCI_fetch_all($statement, $res);
-                // echo "<pre>\n";
-                // var_dump($res);
-                // echo "</pre>\n";
-                // =================================================
-
-                if (!$r) {
-                    echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-                    $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
-                    echo htmlentities($e['message']);
-                    $success = False;
-                }
-                
-                return $statement;
-            }
-            
-            // // Prints the car types passed as a result variable
-            // function printCarTypes($result) {
-            //     echo "<br>Retrieved data from table vehicle_type:<br>";
-            //     echo "<ul>";
-
-            //     while ($row = OCI_Fetch_array($result, OCI_BOTH)) {
-            //         echo "<li>" . $row["VTNAME"];
-            //     }
-                
-            //     echo "</ul>";
-            // }
+            require "Database.php";
 
             // Creates a drop-down menu with all the possible car types
             function createCarTypesDropdown($result) {
@@ -79,9 +19,10 @@
                 echo "</select>";
             }
 
-            connectToDB();
-            $result = executePlainSQL("SELECT * FROM vehicle_type");
-            // printCarTypes($result);
+
+            $db = new Database();
+            $db->connect();
+            $result = $db->executePlainSQL("SELECT * FROM vehicle_type");
             createCarTypesDropdown($result);
         ?>
     </body>
