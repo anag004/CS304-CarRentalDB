64a73427df4eeaf0ec4d042d08e7844b3a6ee548
Ananye Agarwal <ananayagarwal@gmail.com>
2019-11-23 13:45:11 +0530

    Move the function building the queryString for the vehicle to ProjectUtils and make the function so that we can extract multiple kinds of information from the same queryString

--- app/ProjectUtils.php
+++ app/ProjectUtils.php
@@ -1,7 +1,6 @@
 <!-- A drop down menu listing the car types -->
 <?php
-    class  ProjectUtils{
-
+    class  ProjectUtils {
         // Creates a drop-down menu with all the possible types -- including a Anything type
         public static function getDropdownString($result, $name, $class="") {
             $output="";
@@ -14,6 +13,85 @@
             }
             $output .=  "</select>";
             return $output;
+        }
+
+        // Returns an SQL command for the appropriate vehicle
+        public static function getVehicleQueryString($requestObject) {
+            $result = "";
+            $counter = 0; // Stores the number of values specified
+
+            echo "VTNAME: " . $requestObject['VTNAME'];
+
+            // Check for VTNAME
+            if ($requestObject['VTNAME'] != 'all' && $requestObject['VTNAME'] != '') {
+                if ($counter == 0) {
+                    $result = $result . " WHERE ";
+                    $counter++;
+                } else if ($counter >= 1) {
+                    // If there is already something there, add AND
+                    $result = $result . " AND ";
+                    $counter++;
+                } else {
+                    $counter++;
+                }
+                $result = $result . "v.VTNAME = '" . $requestObject['VTNAME'] . "'";
+            }
+
+            // DATE = YYYY-MM-DD
+            // TIME = HH:MM
+            if ($requestObject['FROM_DATE']) {
+                if ($counter == 0) {
+                    $result = $result . " WHERE ";
+                    $counter++;
+                } else if ($counter >= 1) {
+                    // If there is already something there, add AND
+                    $result = $result . " AND ";
+                    $counter++;
+                } else {
+                    $counter++;
+                }
+
+                if (!$requestObject['FROM_TIME']) {
+                    echo "DID NOT FIND FROM_TIME";
+                    return false;
+                }
+
+                $date_format = "'YYYY-MM-DD:HH24:MI'";
+                $from_date = "'" . $requestObject['FROM_DATE'] . ':' . $requestObject['FROM_TIME'] . "'";
+                
+                if (!$requestObject['TO_DATE']) {
+                    echo "DID NOT FIND TO_DATE";
+                    return false;
+                }
+
+                if (!$requestObject['TO_TIME']) {
+                    echo "DID NOT FIND TO_TIME";
+                    return false;
+                }
+
+                $to_date = "'" . $requestObject['TO_DATE'] . ':' . $requestObject['TO_TIME'] . "'";
+
+                $result = $result . "NOT EXISTS ( SELECT  * FROM rentals r WHERE (" . "to_date(" . $from_date . ", "  . $date_format . "), to_date(" . $to_date . ", " . $date_format . ")) OVERLAPS (r.FROM_DATETIME, r.TO_DATETIME) AND r.vlicense = v.vlicense )";
+            } else if ($requestObject['FROM_TIME'] || $requestObject['TO_DATE'] || $requestObject['TO_TIME']) {
+                return false;
+            } 
+
+            if ($requestObject['LOCATION'] != 'all' && $requestObject['LOCATION'] != '') {
+                if ($counter == 0) {
+                    $result = $result . " WHERE ";
+                    $counter++;
+                } else if ($counter >= 1) {
+                    // If there is already something there, add AND
+                    $result = $result . " AND ";
+                    $counter++;
+                } else {
+                    $counter++;
+                }
+                $result = $result . "v.LOCATION = '" . $requestObject['LOCATION'] . "'"; 
+            }
+
+            // echo $result;
+            return $result;
         }
     }
 ?>
--- app/customer/view_vehicles.php
+++ app/customer/view_vehicles.php
@@ -10,6 +10,7 @@
         <?php 
             require "../Database.php";
             require "../ProjectUtils.php";
+
             $db = new Database();
             $db->connect();
             $result = $db->executePlainSQL("SELECT * FROM vehicle_types");
@@ -42,85 +43,6 @@
         <hr>
     </form> 
     <?php
-        // Looks at the $_GET variable and returns the relevant queryString
-        function getQueryString() {
-            $result = "SELECT * FROM vehicles v";
-            $counter = 0; // Stores the number of values specified
-
-            echo "VTNAME: " . $_GET['VTNAME'];
-
-            // Check for VTNAME
-            if ($_GET['VTNAME'] != 'all' && $_GET['VTNAME'] != '') {
-                if ($counter == 0) {
-                    $result = $result . " WHERE ";
-                    $counter++;
-                } else if ($counter >= 1) {
-                    // If there is already something there, add AND
-                    $result = $result . " AND ";
-                    $counter++;
-                } else {
-                    $counter++;
-                }
-                $result = $result . "v.VTNAME = '" . $_GET['VTNAME'] . "'";
-            }
-
-            // DATE = YYYY-MM-DD
-            // TIME = HH:MM
-            if ($_GET['FROM_DATE']) {
-                if ($counter == 0) {
-                    $result = $result . " WHERE ";
-                    $counter++;
-                } else if ($counter >= 1) {
-                    // If there is already something there, add AND
-                    $result = $result . " AND ";
-                    $counter++;
-                } else {
-                    $counter++;
-                }
-
-                if (!$_GET['FROM_TIME']) {
-                    echo "DID NOT FIND FROM_TIME";
-                    return false;
-                }
-
-                $date_format = "'YYYY-MM-DD:HH24:MI'";
-                $from_date = "'" . $_GET['FROM_DATE'] . ':' . $_GET['FROM_TIME'] . "'";
-                
-                if (!$_GET['TO_DATE']) {
-                    echo "DID NOT FIND TO_DATE";
-                    return false;
-                }
-
-                if (!$_GET['TO_TIME']) {
-                    echo "DID NOT FIND TO_TIME";
-                    return false;
-                }
-
-                $to_date = "'" . $_GET['TO_DATE'] . ':' . $_GET['TO_TIME'] . "'";
-
-                $result = $result . "NOT EXISTS ( SELECT  * FROM rentals r WHERE (" . "to_date(" . $from_date . ", "  . $date_format . "), to_date(" . $to_date . ", " . $date_format . ")) OVERLAPS (r.FROM_DATETIME, r.TO_DATETIME) AND r.vlicense = v.vlicense )";
-            } else if ($_GET['FROM_TIME'] || $_GET['TO_DATE'] || $_GET['TO_TIME']) {
-                return false;
-            } 
-
-            if ($_GET['LOCATION'] != 'all' && $_GET['LOCATION'] != '') {
-                if ($counter == 0) {
-                    $result = $result . " WHERE ";
-                    $counter++;
-                } else if ($counter >= 1) {
-                    // If there is already something there, add AND
-                    $result = $result . " AND ";
-                    $counter++;
-                } else {
-                    $counter++;
-                }
-                $result = $result . "v.LOCATION = '" . $_GET['LOCATION'] . "'"; 
-            }
-
-            // echo $result;
-            return $result;
-        }
-
         // Prints the result in an HTML table
         function displayResult($result) {
             $counter = 0;
@@ -150,7 +72,7 @@
         }
         
         if ($_GET['FETCH_DATA'] == "true") {
-            $queryString = getQueryString();
+            $queryString = "SELECT * FROM vehicles v" . ProjectUtils::getVehicleQueryString($_GET);
             if (!$queryString) {
                 echo "ERROR: Invalid request for vehicle list";
             } else {
