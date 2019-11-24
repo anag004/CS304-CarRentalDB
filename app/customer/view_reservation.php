07d0b61c38b106328db6c5e9b55139aaf53b5ec7
Kabir <kabirtomer@gmail.com>
2019-11-24 15:56:53 -0800

    Style reservation reciept

--- app/customer/view_reservation.php
+++ app/customer/view_reservation.php
@@ -1,32 +1,61 @@
 <html>
-    <head>
-        <title>Reservation Successful!</title>
-    </head>
-    <body>
-        <h3> Congrats! You have reserved a new car </h3>
-        <?php
-            require "../Database.php";
-            require "../ProjectUtils.php";
-            
-            $db = new Database();
-            $db->connect();
+<head>
+    <title>Successful Reservation</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../modal.css">
 
-            // Find a reservation object
-            $result = $db->executePlainSQL("SELECT * FROM reservations WHERE conf_no = '" . $_GET['CONF_NO'] . "'");
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
-            // Print this reservation object
-            if (($row = oci_fetch_row($result)) != false) {
-                echo "<ul>";
-                    echo "<li>CONFIRMATION NUMBER: " . $row[0];
-                    echo "<li>VEHICLE TYPE: " . $row[1];
-                    echo "<li>DRIVER'S LICENSE NUMBER: " . $row[2];
-                    echo "<li>BEGIN DATE/TIME: " . $row[3];
-                    echo "<li>END DATE/TIME: " . $row[4];
-                    echo "<li>LOCATION: " . $row[5];
-                echo "</ul>";
-            } else {
-                echo "Invalid reservation conf_no<br>";
-            }
-        ?>
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
+</head>
+<body style="font-family: 'Roboto Slab', serif; background-color: #008a9f; ">
+    <div class = "row h-100">
+        <div class="col-md-2"></div>
+        <div class = "col-md-8 my-auto">
+            <div class="card rounded shadow shadow-sm">
+                <div class="card-header">
+                <h3 class="mb-0">
+                        Successful Reservation! 
+                        <div class="float-right btn btn-info btn-sm" onclick="window.location.href='../home.php';">
+                            Home
+                        </div>
+                    </h3>
+                </div>
+                <div class="card-body">
+                <?php
+                    require "../Database.php";
+                    require "../ProjectUtils.php";
+                    
+                    $db = new Database();
+                    $db->connect();
+
+                    // Find a reservation object
+                    $result = $db->executePlainSQL("SELECT * FROM reservations WHERE conf_no = '" . $_GET['CONF_NO'] . "'");
+
+                    // Print this reservation object
+                    if (($row = oci_fetch_row($result)) != false) {
+                        echo "<table class='table'>";
+                        $hlist=array("CONFIRMATION NUMBER","VEHICLE TYPE","DRIVER'S LICENSE NUMBER","BEGIN DATE/TIME","END DATE/TIME","LOCATION");
+                        for($i=0; $i<6;$i++){
+                            echo "<tr><td>".$hlist[$i]."</td><td>".$row[$i]."</td></tr>";
+                        }
+                    } else {
+                        echo ProjectUtils::getErrorBox("Invalid reservation conf_no");
+                    }
+                ?>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+       
     </body>
 </html>
