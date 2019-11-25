e4f9d7984176bf0d94b5d51328c636aee6252096
Kabir <kabirtomer@gmail.com>
2019-11-24 16:09:15 -0800

    Add view return skeleton

---
+++ app/clerk/view_return.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+    <title>Successful Return</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../modal.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
+</head>
+<body style="font-family: 'Roboto Slab', serif; background-color: #008a9f; ">
+    <div class = "row h-100">
+        <div class="col-md-2"></div>
+        <div class = "col-md-8 my-auto">
+            <div class="card rounded shadow shadow-sm">
+                <div class="card-header">
+                <h3 class="mb-0">
+                        Successful Return! 
+                        <div class="float-right btn btn-info btn-sm" onclick="window.location.href='../home.php';">
+                            Home
+                        </div>
+                    </h3>
+                </div>
+                <div class="card-body">
+                <?php
+                    require "../Database.php";
+                    require "../ProjectUtils.php";
+                    
+                    $db = new Database();
+                    $db->connect();
+                    
+                ?>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+       
+    </body>
+</html>
